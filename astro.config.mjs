068b85b4586698ca1a84df48be6cc45664e7e286
PrzemekMiros <kontakt@futurewebstudio.pl>
2026-02-21 22:47:06 +0100

    Add section

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import netlify from '@astrojs/netlify';
 import tailwindcss from '@tailwindcss/vite';
 import robots from 'astro-robots-txt';
 import sitemap from 'astro-sitemap';
-import min from 'astro-min';
 import remarkBannerStatic from './src/utils/remarkBannerStatic.js';
 
 export default defineConfig({
@@ -19,14 +18,18 @@ export default defineConfig({
         { userAgent: '*', allow: '/' }
       ],
       sitemap: 'https://mooneso.pl/sitemap-index.xml'
-    }),
-    min()
+    })
   ],
   markdown: {
     remarkPlugins: [remarkBannerStatic],
   },
 
   vite: {
+    cacheDir: "./.vite-cache-build",
+    optimizeDeps: {
+      noDiscovery: true,
+      include: [],
+    },
     plugins: [tailwindcss()],
   },
 });
